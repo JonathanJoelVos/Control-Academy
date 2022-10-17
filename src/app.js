17ff067ef20142cf703460a6ca507238477ab0d8
JonathanJoelVos <jonathanjonyjonathanjoelvos@gmail.com>
2022-10-17 11:34:51 -0300

    :)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import db from './config/dbConnect.js';
 import routes from './routes/index.js';
+import cors from 'cors';
 
 db.on("error", console.log.bind(console, "erro"));
 db.once("open", () => {
@@ -9,7 +10,7 @@ db.once("open", () => {
 
 const app = express();
 routes(app);
-
+app.use(cors({exposedHeaders: ['Authorization']}))
 
 app.get("/", (req, res) => {
     res.send("Dale");
